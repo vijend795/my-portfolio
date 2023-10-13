6cbe1660e8075497ad65f7d0da272f7ec54f4439
vijend795 <107410066+vijend795@users.noreply.github.com>
2023-10-13 21:54:50 +1300

    project initiate in same directory

--- app/page.js
+++ app/page.js
@@ -3,10 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <h1>Vijendra Singh</h1>
-      </div>
-
+    <h1>Vijendra Singh</h1>
     </main>
   )
 }
